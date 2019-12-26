2e98e0cf040a69f279c4b59f85ddfaee0746ed95
Jeremias Junior <Zigooo_@hotmail.com>
2019-12-26 06:22:42 -0300

    Add files via upload

--- hf_datacatch.py
+++ hf_datacatch.py
@@ -10,7 +10,8 @@ import threading
 
 ticker_list = stock_cmpr.load_ticker_list('stock_tickers.csv')
 delay = []
-file_json = [] ## armazena os arquivos 
+
+file_json = [] ## armazena os arquivos
 
 def return_date(): ## retorna lista com data atual
     now = datetime.now()
@@ -18,49 +19,54 @@ def return_date(): ## retorna lista com data atual
     return date
 
 
-def init_data_catcher():
-    for i in range(len(ticker_list)):
-        print(i)
-        date = datetime.now()
-        file_path = './data/'
-        file_name = (str(ticker_list[i])+'@'+str(date.year)+':'+ str(date.month) + ':'+ str(date.day)+':'+str(date.hour)+':'+ str(date.minute)+'.json')
-        print(file_name)
-        file_json.append(open(str(file_path) + str(file_name), 'w'))
-        data = {}
-        data[0] = (str(ticker_list[i])+'@'+str(date.year)+':'+ str(date.month) + ':'+ str(date.day)+':'+str(date.hour)+':'+ str(date.minute))
-        with file_json[i] as outfile:
-            json.dump(data, outfile)
+
+for i in range(len(ticker_list)):
+    print(i)
+    global file_json
+    date = datetime.now()
+    file_path = './data/'
+    file_name = (str(ticker_list[i])+'@'+str(date.year)+':'+ str(date.month) + ':'+ str(date.day)+':'+str(date.hour)+':'+ str(date.minute)+'.json')
+    file_json.append(open(str(file_path) + str(file_name), 'w'))
+    data = {}
+    data[0] = (str(ticker_list[i])+'@'+str(date.year)+':'+ str(date.month) + ':'+ str(date.day)+':'+str(date.hour)+':'+ str(date.minute))
+    file_json
+    with file_json[i] as outfile:
+        json.dump(data, outfile)
     return 0
 
 init_data_catcher()
-print('parte 2 @@@@@@@@@@@@@@@@')
+#print(file_json)
+
+f1 = open('test.txt', 'w')
+print(f1.closed)
+
+print(file_json[0].closed)
 iterator = 0
-for t in ticker_list: 
-    
-    print(t)
-    t_0 = datetime.now()
+for t in range(len(ticker_list)):
 
-    finance = yf(t)
+    finance = yf(ticker_list[t])
     summary = finance.get_summary_data()
-
     now = datetime.now()
     summary = finance.get_summary_data()
-    curr_price = finance.get_current_price()
-    curr_volume = finance.get_current_volume()
-    delta_price = finance.get_current_change()
-    curr_bid = summary[t]['bid']
-    curr_ask = summary[t]['ask']    
-    
-    now = datetime.now()
-    time_data = [now.hour, now.min, now.second, now.microsecond]
-    
-    iterator += 1
-    t_1 = datetime.now()
 
-    t_sec = t_1.second - t_0.second
-    t_net = t_1- t_0
+    data = {}
+    data['time'] = return_date()
+    #data['curr_price'] = finance.get_current_price()
+    #data['curr_volume'] = finance.get_current_volume()
+    #data['delta_price'] = finance.get_current_change()
+    #data['curr_bid'] = summary[ticker_list[t]]['bid']
+    #data['curr_ask'] = summary[ticker_list[t]]['ask']
 
-    #sleep(5 - t_sec)
+    #print (file_json[t])
 
-    print(t_net)    
-
+    with file_json[t] as outfile:
+        print(outfile)
+"""
+    now = datetime.now(),
+    summary = finance.get_summary_data(),
+    curr_price = finance.get_current_price(),
+    curr_volume = finance.get_current_volume(),
+    delta_price = finance.get_current_change(),
+    curr_bid = summary[t]['bid'],
+    curr_ask = summary[t]['ask'],
+"""
--- thread_sample.py
+++ thread_sample.py
@@ -9,12 +9,32 @@ Created on Mon Dec 23 00:52:16 2019
 import numpy as np
 import threading
 import time
-start_time = time.time()
+import concurrent.futures
+
+start = time.perf_counter()
 
 
-def do_something():
-    time.sleep(1)
-    print('done sleeping')
+def do_something(seconds):
+    print('Sleeping...')
+    time.sleep(seconds)
+    return 'Done Sleeping...'
 
-t1 = threading.Thread(target=do_something)
-t2 = threading.Thread(target=do_something)
+
+with concurrent.futures.ThreadPoolExecutor() as executor:  #context management
+    result = [executor.submit(do_something, 1) for _ in range(10)]
+    for f in concurrent.futures.as_completed(result):
+        print(f.result())
+
+threads = []
+
+#for _ in range(100):
+    #t = threading.Thread(target=do_something, args=[2])
+    #t.start()
+    #threads.append(t)
+
+#for thread in threads:
+#    thread.join()
+    
+finish = time.perf_counter()
+
+print(f'finished in {round(finish-start,2)} seconds ')
