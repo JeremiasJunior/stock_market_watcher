dd4e82fd6209e8ad9c5345e1e174541859b3fa66
jeremiasjunior <zigooo_@hotmail.com>
2020-02-19 13:54:47 -0300

    servidor e cliente pra coleta dos dados feito

--- RNN/Recurrent_Neural_Networks/rnn.py
+++ RNN/Recurrent_Neural_Networks/rnn.py
@@ -1,39 +1,65 @@
-# Recurrent Neural Network
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  1 20:30:25 2020
 
+@author: gbson
+"""
 
+# Recurrent Neural Networks
 
-# Part 1 - Data Preprocessing
-
-# Importing the libraries
+# Importing Libraries:
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# changing working directory
+os.chdir('/home/gbson/Downloads/Recurrent_Neural_Networks/')
+
 # Importing the training set
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
-training_set = dataset_train.iloc[:, 1:2].values
+training_set = dataset_train.iloc[:, 1:2].values # numpy array da
+# variável 'open' do dataset.
 
 # Feature Scaling
-from sklearn.preprocessing import MinMaxScaler
-sc = MinMaxScaler(feature_range = (0, 1))
+from sklearn.preprocessing import MinMaxScaler # para normalização
+sc = MinMaxScaler(feature_range = (0, 1)) # todos os nossos
+# preços serão escalonados entre 0 e 1 (utilizando o método da
+# normalização).
 training_set_scaled = sc.fit_transform(training_set)
 
 # Creating a data structure with 60 timesteps and 1 output
+# X_train = [] -> uma lista vazia
 X_train = []
 y_train = []
 for i in range(60, 1258):
     X_train.append(training_set_scaled[i-60:i, 0])
     y_train.append(training_set_scaled[i, 0])
+    # no código acima, pegaremos todos os dados escalonados históricos 60 dias
+    # antes de x dia para analisarmos alguns padrões e colocaremos num vetor.
+
 X_train, y_train = np.array(X_train), np.array(y_train)
+# o código acima organiza os vetores do loop 'for' acima em umas colunas aí
+
+# X_train = 'valor de open de 60 dias antes do valor do qual queremos prever
+# y_train = 'valor que queremos prever com X_train'
+
+
 
 # Reshaping
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+# X_train.shape[0] = número de linhas
+# X_train.shape[1] = número de 'timesteps' (colunas)
+# 1 = input da nossa rede (neste caso, número de indicadores de stock market)
+
+# 3D tensor with shape = (batch_size, timesteps, input_size)
+# input_size pode ser os indicadores para o stock_price
 
 
 
-# Part 2 - Building the RNN
-
-# Importing the Keras libraries and packages
+# Part 2 -Building the RNN
+# Importing the Keras Libraries and Packages
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
@@ -43,66 +69,8 @@ from keras.layers import Dropout
 regressor = Sequential()
 
 # Adding the first LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
-regressor.add(Dropout(0.2))
+regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1))) # primeira LSTM layer | .add adiciona uma layer
+regressor.add(Dropout(0.2)) # 20% dos neurônios da LSTM layer serão ignorados de
+# cada 'iteration' do treino
 
-# Adding a second LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
-regressor.add(Dropout(0.2))
-
-# Adding a third LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
-regressor.add(Dropout(0.2))
-
-# Adding a fourth LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50)) # não retorna nenhum valor para o início da NN
-regressor.add(Dropout(0.2))
-
-# Adding the output layer
-regressor.add(Dense(units = 1)) # units = 1 | pois só queremos 1 output
-
-# Compiling the RNN
-regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
-# loss = 'mean_squared_error' | pois estamos fazendo uma regressão
-
-# Keras nos recomenda RMSprop como optimizer de RNNs, porém 'adam' tem melhor
-# performance neste modelo
-
-
-
-# Fitting the RNN to the Training set
-regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
-# batch_size = número de dados forward-propagated antes de ocorrer uma back-propagation
-
-
-
-# Part 3 - Making the predictions and visualising the results
-
-# Getting the real stock price of 2017
-dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
-real_stock_price = dataset_test.iloc[:, 1:2].values
-
-# Getting the predicted stock price of 2017
-dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis = 0)
-inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
-inputs = inputs.reshape(-1,1)
-inputs = sc.transform(inputs) # escalonará os 'inputs' para a escala que os
-# dados treinados foram escalonados
-X_test = []
-for i in range(60, 80):
-    X_test.append(inputs[i-60:i, 0])
-X_test = np.array(X_test)
-X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-predicted_stock_price = regressor.predict(X_test)
-predicted_stock_price = sc.inverse_transform(predicted_stock_price) # este
-# comando desescalona os dados escalonados.
-
-
-# Visualising the results
-plt.plot(real_stock_price, color = 'red', label = 'Real Google Stock Price')
-plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Google Stock Price')
-plt.title('Google Stock Price Prediction')
-plt.xlabel('Time')
-plt.ylabel('Google Stock Price')
-plt.legend()
-plt.show()
+# Adding a second LSTM layer and some Dropout regularization
